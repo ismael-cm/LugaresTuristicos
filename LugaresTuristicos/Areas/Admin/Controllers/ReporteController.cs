d090c3a753eef83836424174882ddfe926cc3a70
Ismael Castillo <ismael.castillo19@itca.edu.sv>
2023-06-15 04:30:54 -0600

    Agregar Reportes al proyecto

---
+++ LugaresTuristicos/Areas/Admin/Controllers/ReporteController.cs
@@ -0,0 +1,153 @@
+﻿using LugaresTuristicos.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.Diagnostics.Metrics;
+
+namespace LugaresTuristicos.Areas.Admin.Controllers
+{
+
+    [Area("admin")]
+    [Route("admin/[controller]/[action]")]
+    [Authorize(Roles = "ADMINISTRADOR")]
+    public class ReporteController : Controller
+    {
+        SitesContext _dbContext = new SitesContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ReporteCategorias(string categoria)
+        {
+            var reportGenerator = new Reports();
+            var filePath = "archivo.pdf";
+
+
+            reportGenerator.GenerarPdfReporteClientes(filePath, _dbContext);
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/pdf", "ReporteClientes.pdf");
+        }
+
+        public IActionResult LugaresMejorPuntuados()
+        {
+            List<Departamento> deptos = new List<Departamento>();
+            deptos = _dbContext.Departamentos.ToList();
+
+            return View(deptos);
+        }
+
+        [HttpPost]
+        public IActionResult LugaresMejorPuntuados(string idDepartamento)
+        {
+            if (idDepartamento.IsNullOrEmpty() && !(idDepartamento is int))
+            {
+                return RedirectToAction("LugaresMejorPuntuados");
+            }
+
+            var reportGenerator = new Reports();
+            var filePath = "archivo.pdf";
+
+
+            reportGenerator.LugaresMejorPuntuados(filePath, _dbContext, int.Parse(idDepartamento));
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/pdf", "Departamentos_Puntuados.pdf");
+        }
+
+        public IActionResult LugaresPorCategoria()
+        {
+            List<Categoria> categorias = new List<Categoria>();
+            categorias = _dbContext.Categorias.ToList();
+
+            return View(categorias);
+        }
+
+        [HttpPost]
+        public IActionResult LugaresPorCategoria(string idCategoria)
+        {
+            if (idCategoria.IsNullOrEmpty() && !(idCategoria is int))
+            {
+                return RedirectToAction("LugaresPorCategoria");
+            }
+
+            var reportGenerator = new Reports();
+            var filePath = "archivo.pdf";
+
+
+            reportGenerator.LugaresPorCategoria(filePath, _dbContext, int.Parse(idCategoria));
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/pdf", "Departamentos_categoria.pdf");
+        }
+
+        public IActionResult LugaresPorMunicipio()
+        {
+            List<Municipio> municipios = new List<Municipio>();
+            municipios = _dbContext.Municipios.ToList();
+
+            return View(municipios);
+        }
+
+        [HttpPost]
+        public IActionResult LugaresPorMunicipio(string idMunicipio)
+        {
+            if (idMunicipio.IsNullOrEmpty() && !(idMunicipio is int))
+            {
+                return RedirectToAction("LugaresPorMunicipio");
+            }
+
+            var reportGenerator = new Reports();
+            var filePath = "archivo.pdf";
+
+
+            reportGenerator.LugaresPorMunicipio(filePath, _dbContext, int.Parse(idMunicipio));
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/pdf", "Departamentos_municipio.pdf");
+        }
+
+        public IActionResult Emprendedores()
+        {
+            var reportGenerator = new Reports();
+            var filePath = "archivo.pdf";
+
+
+            reportGenerator.Emprendedores(filePath, _dbContext);
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/pdf", "emprendedores.pdf");
+        }
+
+
+        public IActionResult LugaresGratuitos()
+        {
+            List<Departamento> deptos = new List<Departamento>();
+            deptos = _dbContext.Departamentos.ToList();
+
+            return View(deptos);
+        }
+
+        [HttpPost]
+        public IActionResult LugaresGratuitos(string idDepartamento)
+        {
+            if (idDepartamento.IsNullOrEmpty() && !(idDepartamento is int))
+            {
+                return RedirectToAction("LugaresGratuitos");
+            }
+
+            var reportGenerator = new Reports();
+            var filePath = "archivo.pdf";
+
+
+            reportGenerator.LugaresGratuitos(filePath, _dbContext, int.Parse(idDepartamento));
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/pdf", "Lugares_gratuitos.pdf");
+        }
+    }
+}
---
+++ LugaresTuristicos/Models/Reports.cs
@@ -0,0 +1,354 @@
+﻿
+using System.Text;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace LugaresTuristicos.Models
+{
+    public class Reports
+    {
+        public void GenerarPdfReporteClientes(string ruta, SitesContext _dbContext)
+        {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(ruta, FileMode.Create));
+            document.Open();
+            //Agregando contenido
+            PdfPTable table = new PdfPTable(3);
+            table.WidthPercentage = 100;
+
+            var data = _dbContext.Categorias.ToList();
+
+            // Agregando contenido a la tabla
+            this.CellHeaderTable("ID", document, table);
+            this.CellHeaderTable("Name", document, table);
+            this.CellHeaderTable("state", document, table);
+
+            foreach (var row in data)
+            {
+                
+                table.AddCell(row.IdCategoria.ToString());
+                table.AddCell(row.NombreCategoria.ToString());
+                table.AddCell(row.Estado.ToString());
+            }
+            this.Header("Reporte Categorias", document);
+
+            //Logo
+            string ruta_logo = "wwwroot/images/Logo.png";
+            iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(ruta_logo);
+            logo.ScaleToFit(100, 100);
+            logo.SetAbsolutePosition(50, 800);
+
+            //Add logo and Items
+            document.Add(logo);
+            document.Add(table);
+            document.Close();
+
+        }
+
+        
+        public void LugaresMejorPuntuados(string ruta, SitesContext _dbContext, int idDepartamento)
+        {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(ruta, FileMode.Create));
+            document.Open();
+            //Agregando contenido
+            PdfPTable table = new PdfPTable(6);
+            float[] columnWidths = { 5f, 35f, 15f, 15f, 15f, 15f }; 
+            table.SetWidths(columnWidths);
+            table.WidthPercentage = 100;
+            //var data = _dbContext.Lugares.Where(l => l.).ToList();
+
+            var data = (from l in _dbContext.Lugares
+                        join m in _dbContext.Municipios on l.IdMunicipio equals m.IdMunicipio
+                        join d in _dbContext.Departamentos on m.IdDepto equals d.IdDepto
+                        where m.IdDepto == idDepartamento
+                        orderby l.LugaresValoraciones.Average(x => x.IdValoracion) descending
+                        select new
+                        {
+                            l.IdLugar,
+                            l.NombreLugar,
+                            l.IdCategoriaNavigation.NombreCategoria,
+                            l.LugaresValoraciones,
+                            l.Precio,
+                            m.Municipio1,
+                            d.Departamento1
+                        });
+
+            // Agregando contenido a la tabla
+            this.CellHeaderTable("ID", document, table);
+            this.CellHeaderTable("Nombre", document, table);
+            this.CellHeaderTable("Categoría", document, table);
+            this.CellHeaderTable("Valoración", document, table);
+            this.CellHeaderTable("Precio", document, table);
+            this.CellHeaderTable("Municipio", document, table);
+
+            foreach (var row in data)
+            {
+
+                table.AddCell(row.IdLugar.ToString());
+                table.AddCell(row.NombreLugar.ToString());
+                table.AddCell(row.NombreCategoria.ToString());
+                table.AddCell(((double)row.LugaresValoraciones.Average(x => x.IdValoracion)).ToString("0.00"));
+                table.AddCell("$" + ((double)row.Precio).ToString("0.00"));
+                table.AddCell(row.Municipio1.ToString());
+
+            }
+            this.Header("Lugares Mejor puntuados", document);
+
+            //Logo
+            string ruta_logo = "wwwroot/images/Logo.png";
+            iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(ruta_logo);
+            logo.ScaleToFit(100, 100);
+            logo.SetAbsolutePosition(50, 800);
+
+            //Add logo and Items
+            document.Add(logo);
+            document.Add(table);
+            document.Close();
+
+        }
+
+        public void LugaresPorCategoria(string ruta, SitesContext _dbContext, int idCategoria)
+        {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(ruta, FileMode.Create));
+            document.Open();
+            //Agregando contenido
+            PdfPTable table = new PdfPTable(6);
+            float[] columnWidths = { 5f, 35f, 15f, 15f, 15f, 15f };
+            table.SetWidths(columnWidths);
+            table.WidthPercentage = 100;
+            //var data = _dbContext.Lugares.Where(l => l.).ToList();
+
+            var data = _dbContext.Lugares.Where(l => l.IdCategoria == idCategoria)
+                .Include(l => l.IdCategoriaNavigation)
+                .Include(l => l.IdMunicipioNavigation)
+                .Include(l => l.LugaresValoraciones)
+                .ToList();
+
+            // Agregando contenido a la tabla
+            this.CellHeaderTable("ID", document, table);
+            this.CellHeaderTable("Nombre", document, table);
+            this.CellHeaderTable("Categoría", document, table);
+            this.CellHeaderTable("Valoración", document, table);
+            this.CellHeaderTable("Precio", document, table);
+            this.CellHeaderTable("Municipio", document, table);
+
+            foreach (var row in data)
+            {
+
+                table.AddCell(row.IdLugar.ToString());
+                table.AddCell(row.NombreLugar.ToString());
+                table.AddCell(row.IdCategoriaNavigation.NombreCategoria.ToString());
+                table.AddCell(((double)row.LugaresValoraciones.Average(x => x.IdValoracion)).ToString("0.00"));
+                table.AddCell("$" + ((double)row.Precio).ToString("0.00"));
+                table.AddCell(row.IdMunicipioNavigation.Municipio1.ToString());
+
+            }
+            this.Header("Lugares por Categoría", document);
+
+            //Logo
+            string ruta_logo = "wwwroot/images/Logo.png";
+            iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(ruta_logo);
+            logo.ScaleToFit(100, 100);
+            logo.SetAbsolutePosition(50, 800);
+
+            //Add logo and Items
+            document.Add(logo);
+            document.Add(table);
+            document.Close();
+
+        }
+
+        public void LugaresPorMunicipio(string ruta, SitesContext _dbContext, int idMunicipio)
+        {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(ruta, FileMode.Create));
+            document.Open();
+            //Agregando contenido
+            PdfPTable table = new PdfPTable(6);
+            float[] columnWidths = { 5f, 35f, 15f, 15f, 15f, 15f };
+            table.SetWidths(columnWidths);
+            table.WidthPercentage = 100;
+            //var data = _dbContext.Lugares.Where(l => l.).ToList();
+
+            var data = _dbContext.Lugares.Where(l => l.IdMunicipio == idMunicipio)
+                .Include(l => l.IdCategoriaNavigation)
+                .Include(l => l.IdMunicipioNavigation)
+                .Include(l => l.LugaresValoraciones)
+                .ToList();
+
+            // Agregando contenido a la tabla
+            this.CellHeaderTable("ID", document, table);
+            this.CellHeaderTable("Nombre", document, table);
+            this.CellHeaderTable("Categoría", document, table);
+            this.CellHeaderTable("Valoración", document, table);
+            this.CellHeaderTable("Precio", document, table);
+            this.CellHeaderTable("Municipio", document, table);
+
+            foreach (var row in data)
+            {
+
+                table.AddCell(row.IdLugar.ToString());
+                table.AddCell(row.NombreLugar.ToString());
+                table.AddCell(row.IdCategoriaNavigation.NombreCategoria.ToString());
+                table.AddCell(((double)row.LugaresValoraciones.Average(x => x.IdValoracion)).ToString("0.00"));
+                table.AddCell("$" + ((double)row.Precio).ToString("0.00"));
+                table.AddCell(row.IdMunicipioNavigation.Municipio1.ToString());
+
+            }
+            this.Header("Lugares por Municipio", document);
+
+            //Logo
+            string ruta_logo = "wwwroot/images/Logo.png";
+            iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(ruta_logo);
+            logo.ScaleToFit(100, 100);
+            logo.SetAbsolutePosition(50, 800);
+
+            //Add logo and Items
+            document.Add(logo);
+            document.Add(table);
+            document.Close();
+
+        }
+
+        public void Emprendedores(string ruta, SitesContext _dbContext)
+        {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(ruta, FileMode.Create));
+            document.Open();
+            //Agregando contenido
+            PdfPTable table = new PdfPTable(4);
+            float[] columnWidths = { 10f, 30f, 40f, 20f}; // Anchos de columna personalizados
+            table.SetWidths(columnWidths);
+
+            table.WidthPercentage = 100;
+            //var data = _dbContext.Lugares.Where(l => l.).ToList();
+
+            var data = _dbContext.Usuarios.Where(x => x.Rol.NombreRol == "EMPRENDEDOR")
+                .Include(u => u.Lugares)
+                .ToList();
+
+            // Agregando contenido a la tabla
+            this.CellHeaderTable("ID", document, table);
+            this.CellHeaderTable("Nombre", document, table);
+            this.CellHeaderTable("Correo", document, table);
+            this.CellHeaderTable("Cant. Propiedades", document, table);
+
+            foreach (var row in data)
+            {
+                table.AddCell(row.IdUsuario.ToString());
+                table.AddCell(row.Nombre.ToString());
+                table.AddCell(row.Correo.ToString());
+                table.AddCell(row.Lugares.Count().ToString());
+
+            }
+            this.Header("Emprendedores", document);
+
+            //Logo
+            string ruta_logo = "wwwroot/images/Logo.png";
+            iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(ruta_logo);
+            logo.ScaleToFit(100, 100);
+            logo.SetAbsolutePosition(50, 800);
+
+            //Add logo and Items
+            document.Add(logo);
+            document.Add(table);
+            document.Close();
+
+        }
+
+        public void LugaresGratuitos(string ruta, SitesContext _dbContext, int idDepartamento)
+        {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(ruta, FileMode.Create));
+            document.Open();
+            //Agregando contenido
+            PdfPTable table = new PdfPTable(6);
+            float[] columnWidths = { 5f, 30f, 15f, 15f, 10f, 15f }; // Anchos de columna personalizados
+            table.SetWidths(columnWidths);
+            table.WidthPercentage = 100;
+
+            var data = _dbContext.Lugares.Where(l => l.IdMunicipioNavigation.IdDepto == idDepartamento)
+                .Where(l => l.Precio == 0)
+                .Include(l => l.IdCategoriaNavigation)
+                .Include(l => l.IdMunicipioNavigation)
+                .Include(l => l.LugaresValoraciones)
+                .ToList();
+
+            // Agregando contenido a la tabla
+            this.CellHeaderTable("ID", document, table);
+            this.CellHeaderTable("Nombre", document, table);
+            this.CellHeaderTable("Categoría", document, table);
+            this.CellHeaderTable("Valoración", document, table);
+            this.CellHeaderTable("Precio", document, table);
+            this.CellHeaderTable("Municipio", document, table);
+
+            foreach (var row in data)
+            {
+
+                table.AddCell(row.IdLugar.ToString());
+                table.AddCell(row.NombreLugar.ToString());
+                table.AddCell(row.IdCategoriaNavigation.NombreCategoria.ToString());
+                table.AddCell(((double)row.LugaresValoraciones.Average(x => x.IdValoracion)).ToString("0.00"));
+                table.AddCell("$" + ((double)row.Precio).ToString("0.00"));
+                table.AddCell(row.IdMunicipioNavigation.Municipio1.ToString());
+
+            }
+            this.Header("Lugares por Municipio", document);
+
+            //Logo
+            string ruta_logo = "wwwroot/images/Logo.png";
+            iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(ruta_logo);
+            logo.ScaleToFit(100, 100);
+            logo.SetAbsolutePosition(50, 800);
+
+            //Add logo and Items
+            document.Add(logo);
+            document.Add(table);
+            document.Close();
+
+        }
+        private void CellHeaderTable(string nombre, Document document, PdfPTable table)
+        {
+            PdfPCell cell = new PdfPCell(new Phrase(nombre, new iTextSharp.text.Font(iTextSharp.text
+                .Font.FontFamily.HELVETICA, 12, iTextSharp.text.Font.BOLD, BaseColor.WHITE)));
+            cell.PaddingTop = 5f;
+            cell.PaddingBottom = 5f;
+            cell.BackgroundColor = new iTextSharp.text.BaseColor(33, 51, 99);
+            table.AddCell(cell);
+        }
+
+        private void Header(string titulo, Document document)
+        {
+
+            //Titulo
+            Phrase headerPhrase = new Phrase();
+            Font titleFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 20);
+            Chunk titleChunk = new Chunk(titulo, titleFont);
+            headerPhrase.Add(titleChunk);
+            Paragraph headerParagraph = new Paragraph(headerPhrase);
+            headerParagraph.Alignment = Element.ALIGN_CENTER;
+            
+
+
+            //Fecha
+            Paragraph fecha = new Paragraph(DateTime.Now.ToString());
+            fecha.Alignment = Element.ALIGN_RIGHT;
+            fecha.Add(Environment.NewLine);
+            fecha.Add(Environment.NewLine);
+            fecha.Font = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 17);
+
+
+
+            //Agregar fecha y titulo
+            document.Add(headerParagraph);
+            document.Add(fecha);
+
+        }
+
+    }
+}
